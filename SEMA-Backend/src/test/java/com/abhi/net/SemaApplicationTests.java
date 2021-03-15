68a0ab5e3b352a4a72adf237c684b5ac268017bc
Abhijeet Sinha <abhi.sin87@gmail.com>
2021-03-15 16:29:02 +0530

    First Commit
    1st Commit For Simple Employee Management Application

---
+++ SEMA-Backend/src/test/java/com/abhi/net/SemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
